f909f271ab43e8424774c1cf9594e5ef2b187290
ravalencia <ravalencia@utpl.edu.ec>
2021-08-25 02:47:01 -0500

    first commit

---
+++ Unidad1/variables.py
@@ -0,0 +1,49 @@
+
+#Datos de tipo Numerico
+
+#PEP8
+numero_1 = 1.5
+print (id(numero_1))
+print (type(numero_1))
+
+numero_2 = 10
+print (id(numero_2))
+print (type(numero_2))
+#PEP8 :  Estandares de Codificacion  Comunidad de Python
+
+# Definir Variables de  tipo STRING
+
+cadena_1 = "Hola"
+cadena_2 = "Mundo"
+cadena_3 = "Desde Python"
+
+print("La variable cadena1 : ",type(cadena_1))
+print("La variable cadena2 : ",type(cadena_2))
+print("La variable cadena3 : ",type(cadena_3))
+
+cadena_3 = 10.5
+print("La variable cadena333 : ",type(cadena_3))
+
+cadena_3 = 100
+print("La variable cadena444 : ",type(cadena_3))
+
+# Tipos de Literales que tenemos python : Datos Primitivos
+
+variable_1 = ""  # Variables de String
+variable_2 = ''  # Variables de String
+variable_3 = 10  # Variables  de tipo  Intero
+variable_4 = 10.5 # Variable de tipo Float
+variable_5 = True # variable de tipo bool
+variable_5 = False # variable de tipo bool
+
+
+#Funciones predefinidas de Python
+print() # Sirve para enviar a  consola los mensajes
+id()   #Me devuelve la posicion de menoria en donde esta almacena la variable
+type() # Tipo de dato q contiene la variable
+
+# Para comentar el  codigo  tenemos
+# Comentarios de 1 sola linea con el signo "#"
+# Comentarios de varias lineas con el signo """sumary_line"""
+
+
