d9ba3b17fb41d00743d89d9ab7a9816885a89b2e
Dawin Calle <dacl010811@gmail.com>
2021-08-31 03:11:54 -0500

    Unidad3 Estructuras

---
+++ Unidad2/operadores_asignacion.py
@@ -0,0 +1,44 @@
+#Operadores de Asignacion
+
+# definicion de una variable
+variable_1 = 20
+
+#Suma y Asigna
+variable_1 += 5
+print(type(variable_1))
+print(variable_1) #25
+
+#Resta y Asigna
+variable_1 -= 10
+print(type(variable_1))
+print(variable_1)  #15
+
+#Multiplica y Asigna
+variable_1 *= 10
+print(type(variable_1))
+print(variable_1)  #150
+
+#Divide y Asigna (Operacion basica)
+variable_1 /= 10
+print(type(variable_1))
+print(variable_1)  # float(15.0)
+
+variable_2 = 110
+variable_2 //= 10 #(Operacion de tipo de dato Entero)
+print(type(variable_2))
+print(variable_2)  # int
+
+#Resto
+variable_2 = 6
+variable_2 %= 2 #(Operacion de tipo de dato Entero)
+print(type(variable_2))
+print(variable_2)  # int
+
+
+#Potencia
+variable_3 = 5
+variable_3 **= 2 #(Operacion de tipo de dato Entero)
+
+print(type(variable_3))
+print(variable_3)  # int
+
---
+++ Unidad3/estructuras.py
@@ -0,0 +1,35 @@
+# Python:
+#Sentencia If (True) : bool()
+
+if True:
+    print("Se cumple la condicion")
+    print("A")
+    variable_1 =  12
+
+#Varios If simples Secuenciales
+a = 2
+b = 50
+
+if a==2:
+    print("Es par")
+
+if b==2:
+    print("Es par")
+
+
+# Ifs Anidados
+if b!=5:
+    print("No es multiplo de 5")
+    if a>=2:
+        print("Es par")
+        if a>=2:
+            print("Es par")
+            if a>=2:
+                print("Es par")
+
+#If else
+if b==5:
+    print("Es par")
+else:
+    print("Es impar")
+
